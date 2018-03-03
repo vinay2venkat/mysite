95132e734e53343403010bb24897a2b1923bce25
vinay2venkat <vinay2venkat@gmail.com>
2018-03-03 12:50:53 +0530

    IP Change

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0ho)bl10%acd3c2fhr!8^-1u5uhg4ed#^p!_&b!64foqw88jp0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['52.66.177.142']
 
